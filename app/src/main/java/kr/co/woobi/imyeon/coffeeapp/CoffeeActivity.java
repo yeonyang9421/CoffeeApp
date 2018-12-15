e0759379f7c0ae2e98670080e9c3522e08f16908
yeonyang9421 <chris09+>
2018-12-16 03:07:54 +0900

    7일차 coffee Activity에 옵션메뉴 붙이기!

--- app/src/main/java/kr/co/woobi/imyeon/coffeeapp/CoffeeActivity.java
+++ app/src/main/java/kr/co/woobi/imyeon/coffeeapp/CoffeeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -109,4 +111,13 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
         mIsCream);
         mPriceTexview.setText(result);
     }
-}
+
+//메뉴를 붙이는 부분
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_coffee,menu);
+        return  true;
+        }
+    }
+
