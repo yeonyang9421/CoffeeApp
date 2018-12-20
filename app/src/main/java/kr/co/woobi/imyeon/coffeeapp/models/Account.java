79d57f3bd85b403d9e98cc0e221da4ca392d0b01
yeonyang9421 <chris09+>
2018-12-20 22:44:58 +0900

    11일차  은행앱 연습문제

---
+++ app/src/main/java/kr/co/woobi/imyeon/coffeeapp/models/Account.java
@@ -0,0 +1,44 @@
+package kr.co.woobi.imyeon.coffeeapp.models;
+
+public class Account {
+
+    private String  id;
+    private String password;
+    private  int balance;
+
+    public Account(int balance, String id, String password) {
+        this.id = id;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("조회한 ID : %S\n 조회한 계좌의 잔액 : %d", id, balance );
+
+    }
+}
