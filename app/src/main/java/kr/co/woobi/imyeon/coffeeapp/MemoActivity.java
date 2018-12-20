4704936e14cbcfcbccc5a730cbbc5b80ea4a537c
yeonyang9421 <chris09+>
2018-12-20 19:11:18 +0900

    11일차 메모앱 AlertDialog 까지 - 리스트뷰 온클릭시 내용 수정으로 들어가지지가 않음 사용자 다이얼로그에서 텍스트가 토스트에 뿌려지지 않음

--- app/src/main/java/kr/co/woobi/imyeon/coffeeapp/MemoActivity.java
+++ app/src/main/java/kr/co/woobi/imyeon/coffeeapp/MemoActivity.java
@@ -10,10 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -64,7 +66,6 @@ public class MemoActivity extends AppCompatActivity implements AdapterView.OnIte
         //이벤트
         mMemoListView.setOnItemClickListener(this);
 
-
         //ContextMenu
         registerForContextMenu(mMemoListView);
 
@@ -73,7 +74,7 @@ public class MemoActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK) {
+        if (resultCode == RESULT_OK) {
             String title = data.getStringExtra("title");
             String content = data.getStringExtra("content");
 
@@ -91,7 +92,7 @@ public class MemoActivity extends AppCompatActivity implements AdapterView.OnIte
 
             Log.d(TAG, "onActivityResult : " + title + "," + content);
             Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
-        } else{
+        } else {
             Toast.makeText(this, "취소되었습니다.", Toast.LENGTH_SHORT).show();
         }
     }
@@ -101,8 +102,8 @@ public class MemoActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Memo memo = mMemoList.get(position);
 
-        Intent intent=new Intent(this, Memo2Activity.class);
-        intent.putExtra("id",id);
+        Intent intent = new Intent(this, Memo2Activity.class);
+        intent.putExtra("id", id);
         // 메모 내용이 수정으로 띄워지지가 않음 왜???????
 //        intent.putExtra("memo",memo);
 
@@ -118,11 +119,11 @@ public class MemoActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-     final   AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         switch (item.getItemId()) {
             case R.id.action_delete:
                 //삭제를 누르면 확인을 받고 싶다.
-                AlertDialog.Builder builder= new AlertDialog.Builder(this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("확인");
                 builder.setMessage("정말 삭제하시겠습니다.");
                 builder.setIcon(R.drawable.capuccino);
@@ -134,17 +135,46 @@ public class MemoActivity extends AppCompatActivity implements AdapterView.OnIte
                         deleteMemo(info.id);
                     }
                 });
-              //부정버튼
-                builder.setNegativeButton("취소",null);
+                //부정버튼
+                builder.setNegativeButton("취소", null);
                 builder.show();
+                return true;
+
+            case R.id.action_custom_dialog:
+                showCustomDialog();
                 return true;
             default:
                 return super.onContextItemSelected(item);
         }
     }
 
+    private void showCustomDialog() {
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_login, null, false);
+        final EditText idEditText = view.findViewById(R.id.id_edit);
+        final EditText passEditText = view.findViewById(R.id.password_edit);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("확인");
+        builder.setMessage("정말 삭제하시겠습니다.");
+        builder.setIcon(R.drawable.capuccino);
+
+        //긍정버튼
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String id = idEditText.getText().toString();
+                String pass = passEditText.getText().toString();
+                Toast.makeText(MemoActivity.this, id + "   " + pass, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //부정버튼
+        builder.setNegativeButton("취소", null);
+        builder.setView(R.layout.dialog_login);
+        builder.show();
+    }
+
     private void deleteMemo(long id) {
-        mMemoList.remove((int)id);
+        mMemoList.remove((int) id);
         mAdapter.notifyDataSetChanged();
     }
 }
